fa424a084fda1afa40eb72ca9e25dcfab270df18
Backsungyeon <111503729+Backsungyeon@users.noreply.github.com>
2025-03-20 17:41:05 +0900

    MainActivity.java

---
+++ report/0320/Call/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.scope;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnCall = findViewById(R.id.btnCall);
+
+        btnCall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:01012345678")); // 전화번호 입력
+                startActivity(intent);
+            }
+        });
+    }
+}
+
